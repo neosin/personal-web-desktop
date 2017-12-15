1fb73f006a9542c3dad13b407d23bebf0f56eb75
Rasmus Falk <rf222fu@student.lnu.se>
2017-12-15 11:33:00 +0100

    Can now get the messages from the server

--- src/js/Chat.js
+++ src/js/Chat.js
@@ -6,11 +6,22 @@ class Chat extends DesktopWindow {
 
     this.nickname = null
     this.message = null
+    this.webSocket = new window.WebSocket('ws://vhost3.lnu.se:20080/socket/', 'chat')
+    this.response = null
   }
 
   createChatWindow (title, icon) {
     this.createWindow(title, icon)
     this.currentWindow.classList.add('chat')
+
+    this.getMessages()
+  }
+
+  getMessages () {
+    this.webSocket.addEventListener('message', event => {
+      this.response = JSON.parse(event.data)
+      console.log(this.response.data)
+    })
   }
 }
 
