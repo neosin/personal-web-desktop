fd0de1ed2a4fb469045cee147deda2af471c2b5a
Rasmus Falk <rf222fu@student.lnu.se>
2017-12-15 15:16:11 +0100

    Can now set a nickname in the chat app

--- src/js/Chat.js
+++ src/js/Chat.js
@@ -6,15 +6,61 @@ class Chat extends DesktopWindow {
     super()
 
     this.nickname = null
-    this.webSocket = new window.WebSocket('ws://vhost3.lnu.se:20080/socket/', 'chat')
+    this.webSocket = null
     this.response = null
     this.chatMessageWindow = null
   }
 
   createChatWindow (title, icon) {
     this.createWindow(title, icon)
-    setup.editAppContent('#chat', this.currentWindow)
     this.currentWindow.classList.add('chat')
+
+    this.chooseNickname()
+  }
+
+  sendMessage () {
+    let message = this.currentWindow.querySelector('textarea')
+
+    let data = {
+      type: 'message',
+      data: message.value,
+      username: this.nickname,
+      channel: 'chat',
+      key: 'eDBE76deU7L0H9mEBgxUKVR0VCnq0XBd'
+    }
+
+    this.webSocket.send(JSON.stringify(data))
+    message.value = ''
+  }
+
+  addMessageToWindow () {
+    if (this.response.type === 'message') {
+      let message = document.createElement('p')
+
+      this.chatMessageWindow.appendChild(message)
+      message.textContent = `${this.response.username}: ${this.response.data}`
+    }
+  }
+
+  chooseNickname () {
+    if (!window.localStorage.getItem('chatName')) {
+      setup.editAppContent('#chatName', this.currentWindow)
+
+      let input = this.currentWindow.querySelector('#content input')
+
+      this.currentWindow.querySelector('#set').addEventListener('click', event => {
+        window.localStorage.setItem('chatName', input.value)
+        this.loadChat()
+      })
+    } else {
+      this.loadChat()
+    }
+  }
+
+  loadChat () {
+    this.webSocket = new window.WebSocket('ws://vhost3.lnu.se:20080/socket/', 'chat')
+    this.nickname = window.localStorage.getItem('chatName')
+    setup.editAppContent('#chat', this.currentWindow)
 
     this.chatMessageWindow = this.currentWindow.querySelector('#content')
 
@@ -32,30 +78,6 @@ class Chat extends DesktopWindow {
       this.response = JSON.parse(event.data)
       this.addMessageToWindow()
     })
-  }
-
-  sendMessage () {
-    let message = this.currentWindow.querySelector('textarea')
-
-    let data = {
-      type: 'message',
-      data: message.value,
-      username: 'rasmus',
-      channel: 'chat',
-      key: 'eDBE76deU7L0H9mEBgxUKVR0VCnq0XBd'
-    }
-
-    this.webSocket.send(JSON.stringify(data))
-    message.value = ''
-  }
-
-  addMessageToWindow () {
-    if (this.response.type === 'message') {
-      let message = document.createElement('p')
-
-      this.chatMessageWindow.appendChild(message)
-      message.textContent = `${this.response.username}: ${this.response.data}`
-    }
   }
 }
 
