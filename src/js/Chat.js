c041576f5995ecec6afda4087cae4029e4682c54
Rasmus Falk <rf222fu@student.lnu.se>
2017-12-15 14:11:18 +0100

    Can now send messages to the server

--- src/js/Chat.js
+++ src/js/Chat.js
@@ -1,27 +1,61 @@
 const DesktopWindow = require('./DesktopWindow')
+const setup = require('./setup')
 
 class Chat extends DesktopWindow {
   constructor () {
     super()
 
     this.nickname = null
-    this.message = null
     this.webSocket = new window.WebSocket('ws://vhost3.lnu.se:20080/socket/', 'chat')
     this.response = null
+    this.chatMessageWindow = null
   }
 
   createChatWindow (title, icon) {
     this.createWindow(title, icon)
+    setup.editAppContent('#chat', this.currentWindow)
     this.currentWindow.classList.add('chat')
 
-    this.getMessages()
-  }
+    this.chatMessageWindow = this.currentWindow.querySelector('#content')
 
-  getMessages () {
+    this.webSocket.addEventListener('open', event => {
+      let p = document.createElement('p')
+      p.textContent = 'You are connected!'
+      this.chatMessageWindow.appendChild(p)
+
+      this.currentWindow.querySelector('#send').addEventListener('click', event => {
+        this.sendMessage()
+      })
+    })
+
     this.webSocket.addEventListener('message', event => {
       this.response = JSON.parse(event.data)
-      console.log(this.response.data)
+      this.addMessageToWindow()
     })
+  }
+
+  sendMessage () {
+    let message = this.currentWindow.querySelector('textarea')
+
+    let data = {
+      type: 'message',
+      data: message.value,
+      username: 'rasmus',
+      channel: 'chat',
+      key: 'eDBE76deU7L0H9mEBgxUKVR0VCnq0XBd'
+    }
+
+    this.webSocket.send(JSON.stringify(data))
+    message.value = ''
+  }
+
+  addMessageToWindow () {
+    if (this.response.type === 'message') {
+      let message = document.createElement('p')
+
+      this.chatMessageWindow.appendChild(message)
+      message.textContent = `${this.response.username}: ${this.response.data}`
+    }
   }
 }
 
