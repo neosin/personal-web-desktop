793e699fea1f4120367c7ba97b522af59a3fa1b4
Rasmus Falk <rf222fu@student.lnu.se>
2017-12-19 20:15:19 +0100

    Can now change location in the weather app

--- src/js/Weather.js
+++ src/js/Weather.js
@@ -17,6 +17,21 @@ class Weather extends DesktopWindow {
     this.currentWindow.classList.add('weather')
     setup.editAppContent('#weather', this.currentWindow)
 
+    this.currentWindow.querySelector('#content button').addEventListener('click', event => {
+      this.changeLocation()
+    })
+
+    this.getData()
+  }
+
+  changeLocation () {
+    let selected = this.currentWindow.querySelector('select').value
+
+    let long = selected.slice(0, 9)
+    let lat = selected.slice(10)
+
+    this.url = `https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/${long}/lat/${lat}/data.json`
+
     this.getData()
   }
 
@@ -38,9 +53,6 @@ class Weather extends DesktopWindow {
     let weatherImages = []
     let highestTemp
     let lowestTemp
-    let weatherImg
-
-    console.log(this.response)
 
     times = times.filter(current => {
       current = parseInt(current.validTime.slice(8, 10))
@@ -55,18 +67,14 @@ class Weather extends DesktopWindow {
         if (parameters[j].name === 't') {
           temps.push({time: times[i].validTime, value: parameters[j].values[0]})
         }
-
-        if (parameters[j].name === 'Wsymb2') {
-          weatherImages.push({time: times[i].validTime, value: parameters[j].values[0]})
-        }
       }
     }
 
     temps.sort((a, b) => { return b.value - a.value })
     weatherImages.sort((a, b) => { return a.value - b.value })
 
-    highestTemp = Math.round(temps[0].value)
-    lowestTemp = Math.round(temps[temps.length - 1].value)
+    highestTemp = temps[0].value
+    lowestTemp = temps[temps.length - 1].value
 
     let value = temps.filter(current => {
       let hours = new Date().getHours()
@@ -75,12 +83,10 @@ class Weather extends DesktopWindow {
       return dataHours === hours
     })
 
-    console.log(value)
-
-    this.displayWeather(value[0].value, highestTemp, lowestTemp, weatherImg)
+    this.displayWeather(value[0].value, highestTemp, lowestTemp)
   }
 
-  displayWeather (temp, highestTemp, lowestTemp, weatherImg) {
+  displayWeather (temp, highestTemp, lowestTemp) {
     this.calculateDay()
 
     let day = this.currentWindow.querySelector('#content h1')
@@ -118,30 +124,30 @@ class Weather extends DesktopWindow {
     }
 
     if (month === 1) {
-        month = 'January'
-      } else if (month === 2) {
-        month = 'February'
-      } else if (month === 3) {
-        month = 'March'
-      } else if (month === 4) {
-        month = 'April'
-      } else if (month === 5) {
-        month = 'May'
-      } else if (month === 6) {
-        month = 'June'
-      } else if (month === 7) {
-        month = 'July'
-      } else if (month === 8) {
-        month = 'August'
-      } else if (month === 9) {
-        month = 'September'
-      } else if (month === 10) {
-        month = 'October'
-      } else if (month === 11) {
-        month = 'November'
-      } else if (month === 12) {
-        month = 'December'
-      }
+      month = 'January'
+    } else if (month === 2) {
+      month = 'February'
+    } else if (month === 3) {
+      month = 'March'
+    } else if (month === 4) {
+      month = 'April'
+    } else if (month === 5) {
+      month = 'May'
+    } else if (month === 6) {
+      month = 'June'
+    } else if (month === 7) {
+      month = 'July'
+    } else if (month === 8) {
+      month = 'August'
+    } else if (month === 9) {
+      month = 'September'
+    } else if (month === 10) {
+      month = 'October'
+    } else if (month === 11) {
+      month = 'November'
+    } else if (month === 12) {
+      month = 'December'
+    }
 
     this.date = `${date} ${month}, ${year}`
     this.day = day
