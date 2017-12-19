4236c17ab4861c038c1e4716237a3a4eac923c76
Rasmus Falk <rf222fu@student.lnu.se>
2017-12-19 11:31:17 +0100

    Can get the weather data from SMHI api

--- src/js/Weather.js
+++ src/js/Weather.js
@@ -4,11 +4,31 @@ const setup = require('./setup')
 class Weather extends DesktopWindow {
   constructor () {
     super()
+
+    this.response = null
+    this.url = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.068581/lat/59.329324/data.json'
   }
 
   createWeatherWindow (title, icon) {
     this.createWindow(title, icon)
     this.currentWindow.classList.add('weather')
+
+    this.getData()
+  }
+
+  getData () {
+    window.fetch(this.url)
+    .then(response => {
+      return response.json()
+    })
+    .then(response => {
+      this.response = response
+      this.printData()
+    })
+  }
+
+  printData () {
+    console.log(this.response.timeSeries)
   }
 }
 
